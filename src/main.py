4d4bc874a2fac19b94623cf26d25b174e985a17d
Federico Alberetti <federicoalberetti@Federicos-MBP.fibertel.com.ar>
2025-05-17 18:18:24 -0300

    Cambie el nombre de utils y genere el grafico en dominio temporal y modifiqué main.py llamando al grafico

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 from audio.generadores import generar_ruido_rosa
 from audio.io_audio import guardar_wav, reproducir_audio
+from utils.graficotemporal import graficar_dominio_temporal
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import write
+import sounddevice as sd
+
 
 def main():
     # Generar 5 segundos de ruido rosa
@@ -12,6 +19,10 @@ def main():
     # Reproducir audio
     print("Reproduciendo ruido rosa durante" ,t, "segundos...")
     reproducir_audio(señal)
+
+    #Genera grafico en el dominio temporal de la señal
+
+    graficar_dominio_temporal(señal, frecuencia_muestreo=44100)
 
 if __name__ == "__main__":
     main()
---
+++ src/utils/graficotemporal.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def graficar_dominio_temporal(señal, frecuencia_muestreo=1.0):
+    """
+    Visualiza el dominio temporal de una señal dada como array/list.
+    """
+    tiempo = np.arange(len(señal)) / frecuencia_muestreo
+    plt.figure(figsize=(10, 4))
+    plt.plot(tiempo, señal)
+    plt.title('Dominio Temporal de la Señal')
+    plt.xlabel('Tiempo (s)')
+    plt.ylabel('Amplitud')
+    plt.grid(True)
+    plt.show()
